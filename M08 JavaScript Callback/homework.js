c5eddb608fedda4db059947e6954f32047f15d2e
Nahuel Medina <nahuelmedina2000@gmail.com>
2023-08-25 18:02:28 -0300

    Resuelvo homework M08

--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -1,60 +1,86 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 
 function mayuscula(nombre) {
-   // Esta función recibe un nombre (string).
-   // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
-   // [Ejemplo]: "mario" ----> "Mario".
-   // Tu código:
+  // Esta función recibe un nombre (string).
+  // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
+  // [Ejemplo]: "mario" ----> "Mario".
+  // Tu código:
+  var primerLetraMayuscula = nombre.charAt(0).toUpperCase() + nombre.slice(1);
+  return primerLetraMayuscula;
 }
 
 function invocarCallback(cb) {
-   // Invoca/ejecuta el callback `cb`.
-   // [NOTA]: no debes reotrnar nada.
-   // Tu código:
+  // Invoca/ejecuta el callback `cb`.
+  // [NOTA]: no debes reotrnar nada.
+  // Tu código:
+  cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
-   // En este ejercicio recibirás dos números y un callback.
-   // El callback realiza una operación matemática, por lo que necesita de los dos números.
-   // Retorna el resultado del callback pasándole como parámetros los números.
-   // Tu código:
+  // En este ejercicio recibirás dos números y un callback.
+  // El callback realiza una operación matemática, por lo que necesita de los dos números.
+  // Retorna el resultado del callback pasándole como parámetros los números.
+  // Tu código:
+  var resultadoDeOperacion = cb(num1, num2);
+  return resultadoDeOperacion;
 }
 
 function sumarArray(arrayOfNumbers, cb) {
-   // Recibes un arreglo de números y un callback.
-   // Suma todos los números del arreglo.
-   // Este resultado debes pasárselo como argumento al callback recibido.
-   // [NOTA]: no debes reotrnar nada.
-   // Tu código:
+  // Recibes un arreglo de números y un callback.
+  // Suma todos los números del arreglo.
+  // Este resultado debes pasárselo como argumento al callback recibido.
+  // [NOTA]: no debes reotrnar nada.
+  // Tu código:
+  var numASumar = 0;
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    numASumar += arrayOfNumbers[i];
+  }
+  cb(numASumar);
 }
 
 function forEach(array, cb) {
-   // Recibes un arreglo y un callback.
-   // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
-   // Debes pasarle el elemento como argumento al callback.
-   // Tu código:
+  // Recibes un arreglo y un callback.
+  // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
+  // Debes pasarle el elemento como argumento al callback.
+  // Tu código:
+  for (let i = 0; i < array.length; i++) {
+    cb(array[i]);
+  }
 }
 
 function map(array, cb) {
-   // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
-   // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
-   // Retorna el nuevo arreglo.
-   // Tu código:
+  // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
+  // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
+  // Retorna el nuevo arreglo.
+  // Tu código:
+  var newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    var elemento = cb(array[i]);
+    newArray.push(elemento);
+  }
+  return newArray;
 }
 
 function filter(arrayOfStrings) {
-   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
-   // Luego retorna un nuevo arreglo con estos elementos.
-   // Tu código:
+  // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
+  // Luego retorna un nuevo arreglo con estos elementos.
+  // Tu código:
+  var nuevoArray = [];
+  for (let i = 0; i < arrayOfStrings.length; i++) {
+    if (arrayOfStrings[i].charAt(0) === "a") {
+      nuevoArray.push(arrayOfStrings[i]);
+    }
+  }
+  return nuevoArray;
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   mayuscula,
-   invocarCallback,
-   operacionMatematica,
-   sumarArray,
-   forEach,
-   map,
-   filter,
+  mayuscula,
+  invocarCallback,
+  operacionMatematica,
+  sumarArray,
+  forEach,
+  map,
+  filter,
 };
